679123822bec95619d51208a2c7ad1e57ad5d027
Tony Vithayathil <tony.vithayathil@gmail.com>
2018-05-27 15:26:31 -0400

    Add function to select/apply color

--- designs.js
+++ designs.js
@@ -7,9 +7,10 @@ document.getElementById("submit").addEventListener("click",(e)=>{
 	e.preventDefault();
 	canvas.innerHTML = "";
 	makeGrid();
+	colorPicker();
 });
 
-// function that makes the canvas/table
+// create canvas/table
 let makeGrid = ()=>{
 	let height = document.getElementById("inputHeight").value;
 	const tableRow = document.createElement("tr");
@@ -26,4 +27,15 @@ let makeGrid = ()=>{
 			allTheRows[y].appendChild(tableColumn.cloneNode(true));
 		}
 	});
+}
+
+// obtain/apply selected color
+let colorPicker = ()=>{
+	let allSquares = document.querySelectorAll("td");
+	for(var i=0; i<allSquares.length; i++) {
+	 	allSquares[i].addEventListener("click", function(){
+			const colorSelected = document.getElementById("colorPicker").value;
+			this.style.backgroundColor = colorSelected;
+		});
+	};
 }
